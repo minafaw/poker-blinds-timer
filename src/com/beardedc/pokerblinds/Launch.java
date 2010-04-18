c27209f074be7f30d61deb863ffa945c1f0b7805
MCCARTJ9 <MCCARTJ9@localhost>
2010-04-18 11:51:59 +0100

    First version that countsdown but does nothing when it reaches the countdown.

--- src/com/beardedc/pokerblinds/Launch.java
+++ src/com/beardedc/pokerblinds/Launch.java
@@ -2,20 +2,35 @@ package com.beardedc.pokerblinds;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.SystemClock;
-import android.widget.Chronometer;
+import android.os.CountDownTimer;
+import android.widget.TextView;
 
 public class Launch extends Activity 
 {
+	private TextView m_txtTimer;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) 
     {
         super.onCreate(savedInstanceState);       
         setContentView(R.layout.main);
-
-        Chronometer cn = (Chronometer) findViewById(R.id.BlindTimer);
-        cn.setBase(SystemClock.elapsedRealtime() - 2000);
-        cn.start();
+        // Here get the elements we need
+        m_txtTimer = (TextView)findViewById(R.id.TextTimer);
+        goTimer();
+    }
+    
+    private void goTimer()
+    {
+    	new CountDownTimer(600000, 1000)
+    	{
+    		public void onTick(long msUntilDone)
+    		{
+    			m_txtTimer.setText("Time: " + msUntilDone / 1000);
+    		}
+    		public void onFinish()
+    		{
+    			m_txtTimer.setText("All done");
+    		}
+    	}.start();
     }
 }
