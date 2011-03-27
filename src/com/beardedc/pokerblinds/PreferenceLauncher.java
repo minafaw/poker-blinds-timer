ed11bd5aa489951db40cd08ecc2a7ed31b2181cd
fuzzybear <fuzzybear@localhost>
2011-03-27 19:11:05 +0100

    possibly the comits from justin?

--- src/com/beardedc/pokerblinds/PreferenceLauncher.java
+++ src/com/beardedc/pokerblinds/PreferenceLauncher.java
@@ -8,7 +8,7 @@ import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
 import android.widget.Toast;
 
-public class PreferenceLauncher extends PreferenceActivity {
+public class PreferenceLauncher extends PreferenceActivity implements OnPreferenceClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -16,26 +16,21 @@ public class PreferenceLauncher extends PreferenceActivity {
 		
 		// Get the custom preference
 		Preference customPref = (Preference) findPreference("customPref");
-		ListenPrivate _listener = new ListenPrivate();
 		
-		customPref.setOnPreferenceClickListener(_listener);
+		
+		customPref.setOnPreferenceClickListener(this);
             
     }
     
-    private class ListenPrivate implements OnPreferenceClickListener {
-
-		public boolean onPreferenceClick(Preference preference) {
-			Toast.makeText(getBaseContext(), 
-							"The custom preference has been clicked", Toast.LENGTH_LONG).show();
-			SharedPreferences customSharedPreference = getSharedPreferences(
-		                	"myCustomSharedPrefs", Activity.MODE_PRIVATE);
-			SharedPreferences.Editor editor = customSharedPreference
-			                .edit();
-			editor.putString("myCustomPref", "The preference has been clicked");
-			editor.commit();
-			return true;
-		}
-    	
-    }
+	public boolean onPreferenceClick(Preference preference) {
+		Toast.makeText(getBaseContext(), 
+						"The custom preference has been clicked", Toast.LENGTH_LONG).show();
+		SharedPreferences customSharedPreference = getSharedPreferences(
+		            	"myCustomSharedPrefs", Activity.MODE_PRIVATE);
+		SharedPreferences.Editor editor = customSharedPreference.edit();
+		editor.putString("myCustomPref", "The preference has been clicked");
+		editor.commit();
+		return true;
+	}
     
 }
